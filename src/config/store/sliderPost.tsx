51d9cb81b3e59e8b9d91942f0a4df35341c95c4f
khanhngo.pix@gmail.com <khanhlatoi1998@gmail.com>
2022-11-06 19:46:49 +0700

    u

--- src/config/store/sliderPost.tsx
+++ src/config/store/sliderPost.tsx
@@ -9,9 +9,9 @@ const initialState: ValuePost = {
     title: '',
     content: '',
     banner: 'https://firebasestorage.googleapis.com/v0/b/blog-image-3779d.appspot.com/o/Image%2Fdefault-banner%2Fdefault-banner.jpg?alt=media&token=abc3e029-918e-4f3c-8ead-d930a45d37a1',
-    like: 290,
-    share: 2,
-    view: 1200,
+    like: 0,
+    share: 0,
+    view: 0,
     createDate: ''
 };
 
